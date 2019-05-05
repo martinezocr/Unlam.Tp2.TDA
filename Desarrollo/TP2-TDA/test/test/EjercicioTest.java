64cb8ede82ed0386d232022cf32b4839463cfdaa
martinezocr <oscar19@live.com.ar>
2019-05-05 20:43:17 -0300

    Punto 3-b. Apile 1000000 de Integers en una PilaEstatica y en una PilaDinamica y compare los tiempos de respuesta de estas operaciones en ambos dise ̃nos.

--- Desarrollo/TP2-TDA/test/test/EjercicioTest.java
+++ Desarrollo/TP2-TDA/test/test/EjercicioTest.java
@@ -7,7 +7,7 @@ import tda.*;
 public class EjercicioTest {
 	private final int ciclo= 1000000;
 
-	@Test
+	
 	public void ColaEstaticaTest() {
 		System.out.print("ColaHL: ");
 		ColaHL<Integer> cola = new ColaHL<Integer>();
@@ -19,4 +19,34 @@ public class EjercicioTest {
 		long endTime= System.currentTimeMillis();
 		System.out.println(endTime -startTime);
 	}
+	
+	@Test
+	public void PilasTest() {
+		
+		PilaEstatica<Integer> pilaE = new PilaEstatica<Integer>();
+		PilaDinamica<Integer> pilaD = new PilaDinamica<Integer>();
+		
+		//pila estatica
+		System.out.print("PilaEstatica ->  ");
+		long startTime = System.currentTimeMillis();
+		for(int i=0; i<ciclo; i++) {
+			pilaE.push(i);
+		}
+		long endTime= System.currentTimeMillis();
+		System.out.println("Tiempo: " + (endTime -startTime));
+		//fin pila estatica
+		
+		//pila dinamica
+		System.out.print("PilaDinamica ->  ");
+		startTime = System.currentTimeMillis();
+		for(int i=0; i<ciclo; i++) {
+			pilaD.push(i);
+		}
+		endTime= System.currentTimeMillis();
+		System.out.println("Tiempo: " + (endTime -startTime));
+		// fin pila dinamica
+		
+	}
+	
+	
 }
