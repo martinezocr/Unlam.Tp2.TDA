150b04eaf3be60872d07ea9db1bcf66ed8785717
martinezocr <oscar19@live.com.ar>
2019-05-05 20:47:54 -0300

    Punto 3-c

--- Desarrollo/TP2-TDA/test/test/EjercicioTest.java
+++ Desarrollo/TP2-TDA/test/test/EjercicioTest.java
@@ -48,5 +48,31 @@ public class EjercicioTest {
 		
 	}
 	
-	
+	@Test
+	public void ColasTest() {
+		
+		ColaEstatica<Integer> colaE = new ColaEstatica<Integer>();
+		ColaDinamica<Integer> colaD = new ColaDinamica<Integer>();
+		
+		//pila estatica
+		System.out.print("ColaEstatica ->  ");
+		long startTime = System.currentTimeMillis();
+		for(int i=0; i<ciclo; i++) {
+			colaE.offer(i);
+		}
+		long endTime= System.currentTimeMillis();
+		System.out.println("Tiempo: " + (endTime -startTime));
+		//fin pila estatica
+		
+		//pila dinamica
+		System.out.print("ColaDinamica ->  ");
+		startTime = System.currentTimeMillis();
+		for(int i=0; i<ciclo; i++) {
+			colaD.offer(i);
+		}
+		endTime= System.currentTimeMillis();
+		System.out.println("Tiempo: " + (endTime -startTime));
+		// fin pila dinamica
+		
+	}
 }
